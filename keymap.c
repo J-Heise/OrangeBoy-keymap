0a76f64e6dbc65d2367d38c6a2f85d5cca648b6a
J-Heise <johannes.erik.heise@gmail.com>
2022-02-21 00:59:03 +0100

    improved formatting

--- keymap.c
+++ keymap.c
@@ -18,13 +18,15 @@
 #include QMK_KEYBOARD_H
 
 // Defines names for use in layer keycodes and the keymap
-enum layer_names {
+enum layer_names 
+{
     _BASE,
     _FN1,
 };
 
 // Defines custom keycodes for using macros
-enum custom_keycodes {
+enum custom_keycodes 
+{
     DIA_A = SAFE_RANGE,
     DIA_U,
     DIA_O,
@@ -109,25 +111,28 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 
     default:
     return true;
-  }
 };
 
 //Defines different keyboard layers:
- const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = 
+{
+    //Base Layer:
+    [_BASE] = LAYOUT
+    (
+        KC_MUTE, KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_TRNS, KC_BSPC,
+        KC_PGUP, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+        KC_PGDN, MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+                 KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_UP,  KC_RSFT,
+                 KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,  MO(1),            KC_SPC,           KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT,
+    )
 
-//Base Layer:
-[_BASE] = LAYOUT(
-KC_MUTE, KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_TRNS, KC_BSPC,
-KC_PGUP, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-KC_PGDN, MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,   KC_RSFT,
-         KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,  MO(1),            KC_SPC,           KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT),
-
-//Second Layer (Accessed through pressing MO(1)):
-[_FN1] = LAYOUT(
-KC_TRNS, KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_DEL,
-KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, EURO_KC, KC_TRNS, KC_TRNS, KC_TRNS, DIA_U,   KC_TRNS, DIA_O,   KC_MPLY, KC_TRNS, KC_TRNS, KC_TRNS,
-KC_TRNS, KC_CAPS, DIA_A,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
-         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLU, KC_TRNS,
-         KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT),
+    //Second Layer (Accessed through pressing MO(1)):
+    [_FN1] = LAYOUT
+    (
+        KC_TRNS, KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_DEL,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, EURO_KC, KC_TRNS, KC_TRNS, KC_TRNS, DIA_U,   KC_TRNS, DIA_O,   KC_MPLY, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_CAPS, DIA_A,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLU, KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT,
+    )
 };
